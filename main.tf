e41e8d084b894066b0bb1e2a409fcf6e7621148d
eechava6 <esteban.echavarria@wizeline.com>
2021-09-07 11:04:16 -0500

    Basic ECS cluster template working

--- main.tf
+++ main.tf
@@ -25,3 +25,103 @@ resource "aws_ecs_cluster" "cluster" {
   }
 }
 
+//Application 1
+resource "aws_ecs_task_definition" "service" {
+  family = "service-${var.env}"
+  network_mode = "awsvpc"
+  container_definitions = jsonencode([
+    {
+      name      = "service-${var.env}"
+      image     = "nginxdemos/hello:latest"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+  requires_compatibilities = ["FARGATE"]
+  cpu = 256
+  memory = 512 
+
+  tags = {
+    env       = "${var.env}"
+    terraform = "true"
+  }
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "loadbalance"
+  internal           = false
+  load_balancer_type = "application"
+  subnets = [ "subnet-cf1277a9", "subnet-4c05b316" ]
+  security_groups = [ "sg-4af9fb00" ]
+
+    tags = {
+    env = "${var.env}"
+    terraform = true
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-6603a800"
+  target_type = "ip"
+}
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = aws_lb_listener.listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/static/*"]
+    }
+  }
+}
+
+resource "aws_ecs_service" "service" {
+  
+  name            = "service-${var.env}"
+  cluster         = aws_ecs_cluster.cluster.id
+  task_definition = aws_ecs_task_definition.service.arn
+  desired_count   = 1
+  launch_type = "FARGATE"
+  network_configuration {
+      subnets = [ "subnet-cf1277a9", "subnet-4c05b316" ]
+      security_groups = [ "sg-4af9fb00" ]
+      assign_public_ip = true //False on prod
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "service-${var.env}"
+    container_port   = 80
+  }
+    tags = {
+    env = "${var.env}"
+    terraform = true
+  }
+  depends_on = [
+    aws_lb_listener.listener,
+  ]
+}
